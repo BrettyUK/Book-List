19208185f1d349fb99fae4e429707136d585646a
BrettyUK <ryanbrett@Ryans-MacBook-Pro.local>
2020-05-25 13:08:54 +0100

    built ui and can add book to list

---
+++ app.js
@@ -0,0 +1,47 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constuctor
+function UI() {}
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  // Create element
+  const row = document.createElement('tr');
+  // Insert colls
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a></td>`;
+
+  list.appendChild(row);
+};
+
+// Clear fields
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+};
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // Get form values
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+  // New book
+  const book = new Book(title, author, isbn);
+  // Instantiate UI
+  const ui = new UI();
+  // Add book to list
+  ui.addBookToList(book);
+  // Clear fields
+  ui.clearFields();
+  e.preventDefault();
+});
